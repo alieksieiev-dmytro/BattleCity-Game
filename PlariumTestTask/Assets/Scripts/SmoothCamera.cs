8a21c64f50d3e9ec3622c1625639662680b814a9
alieksieiev-nure <dmytro.alieksieiev.21@gmail.com>
2021-06-22 23:26:52 +0300

    Revert "Revert commit"
    This reverts commit e1ed92a84669ad366bdcdd13470b2ef6207ef0da.

--- PlariumTestTask/Assets/Scripts/SmoothCamera.cs
+++ PlariumTestTask/Assets/Scripts/SmoothCamera.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SmoothCamera : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
     public Transform target;
     public float smoothTime = 0.2f;
 
@@ -13,14 +12,14 @@ public class SmoothCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 targetPosition = new Vector3(player.transform.position.x, player.transform.position.y, -100f);
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
         transform.position = targetPosition;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 targetPosition = new Vector3(player.transform.position.x, player.transform.position.y, -100f);
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
     }
 }
